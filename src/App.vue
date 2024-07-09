7b2530b687dd8764ef1bba7f46eddd44fa947cd3
Arela-Cristina <instagrandine@gmail.com>
2024-07-09 17:10:15 +0200

    Added store.js to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,27 @@ import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppCards from './components/AppCards.vue'
 
+// importiamo il nostro store
+import { store } from './store.js';
+
 export default {
   components: {
     AppHeader,
     AppMain,
     AppCards
-  
-  }
+  },
+  data() { // li diciamo di farci tornare la struttura dati
+    return {
+      store,
+    }
+  },
 }
 </script>
 
 <template>
-   <AppHeader/>
-   <AppMain/>
-   <AppCards/>
-  
+  <AppHeader />
+  <AppMain />
+  <AppCards />
 </template>
 <!-- hemos quitado scope para importar todos nuestros files -->
-<style  lang="scss">
-
-</style>
+<style  lang="scss"></style>
