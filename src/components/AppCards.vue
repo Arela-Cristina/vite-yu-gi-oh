4e3f33a4b091a5598c0701389341b0845601353b
Arela-Cristina <instagrandine@gmail.com>
2024-07-09 15:59:54 +0200

    Added debug static content to App Cards

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -6,9 +6,23 @@ export default {
 </script>
 
 <template>
-    <h1>Saro le cards :v se riescono ad aggiungermi ovviamente :v </h1>
+    <div class="card">
+        <img src="https://i.pinimg.com/736x/0b/3a/35/0b3a3535fce27a486d8b6b9b22144fe5.jpg" alt="drago dorato">
+        <div>Nome Carta</div>
+        <div>Tipo Carta</div>
+    </div>
 </template>
 
 <style scoped>
 
+/* debug */
+.card {
+    background-color: coral;
+    text-align: center;
+    width: calc(100% / 5 - 1rem);
+}
+
+img {
+    width: 100%;
+}
 </style>
