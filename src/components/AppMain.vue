a6d54641e2c165106a480069d98c1ed546ef444d
Arela-Cristina <instagrandine@gmail.com>
2024-07-09 16:17:41 +0200

    import AppCards component on AppMain component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,17 @@
 <script>
+import AppCards from './AppCards.vue'
 
 export default {
     name: "AppMain",
-    
+    components: {
+        AppCards, // componente figlio
+    }
 }
+
 </script>
 
 <template>
     <h1>Sono il Main</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
